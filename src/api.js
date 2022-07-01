18b94cbaedfd7126f78e06ab5ea35e2e7ef5b649
Wosen100 <83084085+Wosen100@users.noreply.github.com>
2022-07-01 07:05:55 -0400

    update api https

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const makeCityQueryUrl = (cityQuery)=> `http://api.openweathermap.org/geo/1.0/direct?q=${cityQuery}&limit=5&appid=${process.env.REACT_APP_WEATHER_KEY}`
+export const makeCityQueryUrl = (cityQuery)=> `https://api.openweathermap.org/geo/1.0/direct?q=${cityQuery}&limit=5&appid=${process.env.REACT_APP_WEATHER_KEY}`
 
 
 export const makeWeatherQueryUrl = (lat,lon) => `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHERBIT_KEY}`
